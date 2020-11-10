61661a4a29fc39bcb22df95ba0b9f275804c609d
AjjlalAhmed <68289247+AjjlalAhmed@users.noreply.github.com>
2020-11-11 01:28:26 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,9 +14,42 @@ burgerBtn.addEventListener("click", () => {
 
 window.addEventListener("scroll", function () {
   const header = document.getElementById("header");
-  if(window.scrollY  == 0){
+  if(window.scrollY  < 100){
     header.style.background = "transparent"
   } else{
     header.style.background = "#000"
   }
 });
+
+const comments = ["Lorem ipsum dolor sit amet consectetur,adipisicing elit. Quotenetur incidunt natus in sed dicta nihil est vero eumconsectetur?",
+
+"survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem",
+
+"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin",]
+const Name = ["william smith","jack","joe"]
+
+
+let count = 0;
+const nextBtn = document.querySelector(".next-btn"); 
+const prevBtn = document.querySelector(".prev-btn");
+nextBtn.addEventListener("click", () => {
+  count++;
+    if(count > 2 ){
+      count = 0
+    }
+    const commentBox = document.querySelector(".comment");
+    const userName = document.querySelector(".user > h1");
+    commentBox.innerHTML = comments[count];
+    userName.innerHTML = Name[count]
+})
+
+prevBtn.addEventListener("click", () => {
+  count--;
+    if(count < 0 ){
+      count = 2
+    }
+    const commentBox = document.querySelector(".comment");
+    const userName = document.querySelector(".user > h1");
+    commentBox.innerHTML = comments[count];
+    userName.innerHTML = Name[count]
+})
